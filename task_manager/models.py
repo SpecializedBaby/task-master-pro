9a9401ead8d3dba6170bb6e3132dbf7dea7e2b61
Dede <specialized8393@gmail.com>
2024-05-24 16:40:26 +0100

    Implementation Models: TaskType, Position, Worker, Task

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, blank=True, null=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Urgent', 'Urgent'),
+        ('High', 'High'),
+        ('Medium', 'Medium'),
+        ('Low', 'Low'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, null=True)
+    assignees = models.ManyToManyField(Worker)
