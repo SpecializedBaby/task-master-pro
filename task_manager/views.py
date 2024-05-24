71e2094e348885936b0ddefd415b44b17465fd6c
Dede <specialized8393@gmail.com>
2024-05-24 17:29:33 +0100

    feat: Implement index view to display statistics on the homepage
    - Added index view to display the number of workers and tasks
    - Implemented session tracking for the number of visits
    - Created a context dictionary to pass statistics to the template
    - Rendered the index view using the 'task_manager/index.html' template
    
    This commit introduces the index view, which provides an overview of the number of workers and tasks in the system. Additionally, it includes session tracking to count the number of visits to the homepage.

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from task_manager.models import Worker, Task, TaskType, Position
+
+
+def index(request):
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
