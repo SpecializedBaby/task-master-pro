0202e4100f0d82e55fd05604b8db98b4c35e7017
Dede <specialized8393@gmail.com>
2024-05-29 00:10:14 +0100

    feat: Redefine get_absolute_url method in Worker model
    - Added get_absolute_url method to the Worker model
    - The method returns the URL for the worker detail view using reverse with the worker's primary key
    
    This commit enhances the Worker model by providing a method to generate the absolute URL for viewing a worker's details, improving navigation and linking within the task management system.

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class TaskType(models.Model):
@@ -20,6 +21,12 @@ class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.SET_NULL, blank=True, null=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+    def get_absolute_url(self):
+        return reverse("task_manager:worker-detail", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Task(models.Model):
